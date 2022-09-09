d16b9af4f56ce50c5f611b533ab41076e33245bd
Finnvb <finnvb@gmail.com>
2022-09-09 19:09:04 +0200

    bla

--- visitekaartje-sveltekit/src/routes/about/+page.svelte
+++ visitekaartje-sveltekit/src/routes/about/+page.svelte
@@ -1,2 +1,16 @@
-
-about page
+<script>
+    import createClient from '$lib/prismicio';
+    import  * as prismicH from '@prismicio/helpers';
+    
+    const client = createClient()
+    const prismicQuery = client.getFirst()
+  </script>
+  
+  {#await prismicQuery}
+    <p>Loading...</p>
+  {:then prismicResponse}
+    <h1>{prismicH.asText(prismicResponse.data.title)}</h1>
+    {@html prismicH.asHTML(prismicResponse.data.description)}
+  {:catch error}
+    <pre>{error.message}</pre>
+  {/await}
